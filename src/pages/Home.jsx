cb4dacef2d1bb9b560aae30aa568d4d43b7644b1
shrishail21 <shrishailbhagat21@gmail.com>
2025-10-05 00:56:06 +0530

    minor change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import SearchBar from '../components/SearchBar';
 import RegisterButton from '../components/RegisterButton';
+import Navbar from '../components/Navbar';
 
 const Home = () => {
     return (
         <div className=" flex flex-col items-center justify-center h-screen w-screen bg-gray-100 space-y-40">
+            <Navbar />
             <SearchBar  />
             <RegisterButton />
         </div>
